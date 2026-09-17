61e781f2bdfe57c8b73c7e7ba0076a84f32d11e0
Thirupathi <raminenithirupathiodl@gmail.com>
2026-09-17 23:15:10 +0530

    numpy questions added

---
+++ numpy.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+arr = np.array([10, 20, 30, 40, 50])
+
+print(arr.sum())
+
+import numpy as np
+
+arr = np.array([25, 10, 45, 30, 15])
+
+print("Maximum:", arr.max())
+print("Minimum:", arr.min())
+
+import numpy as np
+
+arr = np.array([25, 10, 45, 30, 15])
+
+print("Maximum:", arr.max())
+print("Minimum:", arr.min())
